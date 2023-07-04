bea89a8f30f7c68c944f3eea0cd468a36223ccb3
idrobinin <idrobinin@gmail.com>
2023-07-04 15:20:45 +0800

    created content controller logic

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,21 @@ import logger from "morgan";
 
 //routes
 import streamRouter from "./modules/stream/stream.controller";
+import contentRouter from "./modules/content/content.controller";
 
+// middleware
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
 
+// endpoints
 app.use("/stream", streamRouter);
+app.use("/content", contentRouter);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log("Start");
+  console.log("Start streaming");
   console.log(`http://localhost:${PORT}`);
 });
--- src/modules/stream/stream.controller.ts
+++ src/modules/stream/stream.controller.ts
@@ -1,11 +1,48 @@
 import { Router } from "express";
+import WebTorrent from "webtorrent";
 
 const router = Router();
+const client = new WebTorrent();
 
-router.get("/", (request, response) => {
-  response.status(200).send({
-    hello: "world",
+let state = {
+  progress: 0,
+  downloadSpeed: 0,
+  ratio: 0,
+};
+let error;
+
+client.on("error", (err: Error) => {
+  console.error("err", err.message);
+  error = err.message;
+});
+
+// при обновлении торрента обновляем наш стейт
+client.on("torrent", () => {
+  console.log(client.progress);
+  state = {
+    // переводим биты прогресса в число
+    progress: Math.round(client.progress * 100 * 100) / 100,
+    downloadSpeed: client.downloadSpeed,
+    ratio: client.ratio,
+  };
+});
+
+router.get("/add/:magnet", (req, resp) => {
+  const magnet = req.params.magnet;
+
+  client.add(magnet, (torrent) => {
+    const files = torrent.files.map((data) => ({
+      data: data.name,
+      length: data.length,
+    }));
+
+    resp.status(200).send(files);
   });
+});
+
+// ендпойнт для статистики
+router.get("/stats", (request, response) => {
+  response.status(200).send(state);
 });
 
 export default router;
