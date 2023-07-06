ae00d9dd0bce92496446af22eca57868c5b366b4
idrobinin <idrobinin@gmail.com>
2023-07-06 12:55:18 +0800

    added mock of player to html

--- src/modules/stream/stream.controller.ts
+++ src/modules/stream/stream.controller.ts
@@ -1,5 +1,5 @@
-import { Router } from "express";
-import WebTorrent from "webtorrent";
+import { Router, Response, Request, NextFunction } from "express";
+import WebTorrent, { Torrent, TorrentFile } from "webtorrent";
 
 const router = Router();
 const client = new WebTorrent();
@@ -27,7 +27,7 @@ client.on("torrent", () => {
   };
 });
 
-router.get("/add/:magnet", (req, resp) => {
+router.get("/add/:magnet", (req: Request, resp: Response) => {
   const magnet = req.params.magnet;
 
   client.add(magnet, (torrent) => {
@@ -41,8 +41,87 @@ router.get("/add/:magnet", (req, resp) => {
 });
 
 // ендпойнт для статистики
-router.get("/stats", (request, response) => {
-  response.status(200).send(state);
+router.get("/stats", (req: Request, resp: Response) => {
+  resp.status(200).send(state);
 });
 
+// stream endpoint
+interface StreamRequest extends Request {
+  params: {
+    magnet: string;
+    fileName: string;
+  };
+  headers: {
+    range: string;
+  };
+}
+
+interface ErrorWithStatus extends Error {
+  status: number;
+}
+router.get(
+  "/:magnet/:fileName",
+  (req: StreamRequest, res: Response, next: NextFunction) => {
+    const {
+      params: { magnet, fileName },
+      headers: { range },
+    } = req;
+
+    // проверяем наличие range
+    if (!range) {
+      const error = new Error(
+        "Range is not defined. Please make request from HTML5 player"
+      ) as ErrorWithStatus;
+      // устанавливаем нужный статус ошибки
+      error.status = 416;
+      return next(error);
+    }
+
+    const torrentFile = client.get(magnet) as Torrent;
+    // переменная для chunk
+    let file = <TorrentFile>{};
+    // ищем нужный нам файл для стрима по имени и кладем в файл
+    for (let i = 0; i < torrentFile.files.length; i++) {
+      const currentTorrentPiece = torrentFile.files[i];
+      if (currentTorrentPiece.name == fileName) {
+        file = currentTorrentPiece;
+      }
+    }
+
+    //получаем размер файла для стрима
+    const fileSize = file.length;
+
+    // достаем range(start,end - positions) из приходящей строки 'bytes-0-14-1342'
+    const [startParsed, endParsed] = range.replace(/bytes=/, "").split("-");
+
+    const start = Number(startParsed);
+    const end = endParsed ? Number(endParsed) : fileSize - 1;
+
+    // считаем размер чанка
+    const chunkSize = end - start + 1;
+
+    // создаем headers для отправки плееру
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunkSize,
+      "Content-Type": "video/mp4",
+    };
+
+    res.writeHead(206, headers);
+
+    const streamPositions = {
+      start,
+      end,
+    };
+
+    const stream = file.createReadStream(streamPositions);
+
+    stream.pipe(res);
+
+    // обработка ошибки если стрим упадет
+
+    stream.on("error", (err) => next(err));
+  }
+);
 export default router;
